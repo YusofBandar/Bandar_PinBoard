a783e7ab0f3fc8f3000f6cf2079e7e40f9fcdab9
Yusof Bandar <ybandar1902@gmail.com>
2019-02-16 23:31:37 +0000

    Delete domTester.js

--- src/domTester.js
+++
@@ -1,9 +0,0 @@
-const DomElement = require('./DomElement');
-const HeaderElement = require('./HeaderElement');
-
-let domEl = new DomElement();
-let header = new HeaderElement('hello');
-
-
-
-console.log(header.render());
