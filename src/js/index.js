e459841106a0f68aace49f2ebeef6237d7d54322
YusofBandar <ybandar1902@gmail.com>
2020-02-16 18:46:43 +0000

    Generating bookmarks page

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,21 @@
+const fs = require('fs');
+const es = require('event-stream');
+
 const read = require("./read");
 const bookmark = require("./elements/bookmark");
-const ReactDOMServer = require('react-dom/server');
+const page = require("./elements/page");
 
-read.readBookmarks('../bookmarks.md', function (bookmarks) {
-    let children = [];
-    bookmarks.forEach(({link, title}) => {
-        children.push(bookmark.render(link, title));
-    });
-    
-})
+const ReactDOMServer = require("react-dom/server");
+const React = require("react");
+
+read.readBookmarks("../bookmarks.md", function(bookmarks) {
+  const DOM = ReactDOMServer.renderToStaticMarkup(
+    page.render(
+      <React.Fragment>
+        {bookmarks.map(({ link, title }) => bookmark.render(link, title))}
+      </React.Fragment>
+    )
+  );
+
+  fs.writeFileSync("../static/index.html", DOM);
+});
